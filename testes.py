cd3f7c62b25d49979c3047ee678cd116a16863b1
DiogoSoares013 <carlosdiogo.fs@gmail.com>
2024-09-19 22:52:32 -0300

    qualquercoisa

--- testes.py
+++ testes.py
@@ -1,3 +1,11 @@
-x = (19//2)
-x2= 19%2 
-print(f'{x} {x2}')
+
+# regra de 3 testando COTAS DE INVESTIMENTO
+print('UNIDADE!! \n')
+v1 = float(input('Digite o valor da cota '))
+r1 = float(input('Digite quanto que recebe a unidade por essa cota '))
+vx = float(input('Digite quantas cotas voce quer testar '))
+v1C = vx * v1
+
+resx = (r1 * v1C)/v1 
+
+print(f'\nO resultado da regra deu {resx} \nVoce iria gastar {v1C} para receber aquela quantia\n')
